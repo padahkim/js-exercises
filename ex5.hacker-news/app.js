c06200ecd9182e52973e82362db24e5641ea8871
padahkim <padahkim@gmail.com>
2021-11-22 15:25:45 +0900

    paging update

--- ex5.hacker-news/app.js
+++ ex5.hacker-news/app.js
@@ -3,6 +3,10 @@ const NEWS_URL = 'https://api.hnpwa.com/v0/news/1.json';
 const CONTENT_URL ='https://api.hnpwa.com/v0/item/@id.json';
 const container = document.getElementById('root');
 
+const store = {
+  currentPage: 1
+};
+
 function getData(url) {
   ajax.open('GET', url, false); //initializes a newly-created request, or re-initializes an existing one.
   ajax.send();//request를 보냅니다. 요청이 비동기인 경우(기본값), 이 메소드는 요청이 보내진 즉시 반환합니다.
@@ -16,10 +20,10 @@ function newsFeed() {
 
   newsLists.push("<ul>")
 
-  for (let i=0; i<10; i++) {
+  for (let i = (store.currentPage -1)*10; i < store.currentPage*10; i++) {
     newsLists.push(`
     <li>
-      <a href="#${newsFeed[i].id}">
+      <a href="#/show/${newsFeed[i].id}">
       ${newsFeed[i].title}(${newsFeed[i].comments_count})
       </a>
     </li>
@@ -27,19 +31,25 @@ function newsFeed() {
   }
 
   newsLists.push("</ul>");
-
+  
+  newsLists.push(`
+  <div>
+    <a href="#/page/${store.currentPage > 1 ? store.currentPage - 1 : 1}">이전페이지</a>
+    <a href="#/page/${store.currentPage < 3 ? store.currentPage + 1 : 3}">다음페이지</a>
+  </div>
+  `);
+  
   container.innerHTML = newsLists.join("");
 }
 
 function newsDetail() {
-  const id = location.hash.substring(1);
+  const id = location.hash.substring(7);
   const newsContent = getData(CONTENT_URL.replace('@id',id));
 
   container.innerHTML=`
     <h1>${newsContent.title}</h1>
-
     <div>
-      <a href="#">목록으로</a>
+      <a href="#/page/${store.currentPage}">목록으로</a>
     </div>
   `;
 }
@@ -48,6 +58,9 @@ function router() {
   const routePath = location.hash;
 
   if (routePath === "") {
+    newsFeed();
+  } else if(routePath.indexOf("#/page/") >= 0){
+    store.currentPage = Number(routePath.substring(7));
     newsFeed();
   } else {
     newsDetail();
