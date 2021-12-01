5e2fbc056c86417ade08ca48d0d30d6aad4cfbcb
padahkim <padahkim@gmail.com>
2021-12-01 16:25:08 +0900

    habit tracker add button

--- habit-tracker/src/components/habit.jsx
+++ habit-tracker/src/components/habit.jsx
@@ -2,26 +2,19 @@ import React, { Component } from 'react';
 
 
 class Habit extends Component {
-  state = {
-    count:0,
-  };
-
   handleIncrement = () => {
-    this.setState({ count: this.state.count + 1})
-  };
-
-  handleDecrement = () => {
-    const count = this.state.count;
-    
-    this.setState({count: count > 0 ? count - 1 : 0 })
-
-    //this.setState({ count: this.state.count > 0 ? this.state.count - 1 : this.state.count === 0})이거 treu or false값임;;
+    this.props.onIncrement(this.props.habit)
   }
 
+  handleDecrement = () => {
+    this.props.onDecrement(this.props.habit)
+  }
+  
+  handleDelete = () => {
+    this.props.onDelete(this.props.habit)
+  };
+  
   render() {
-    console.log(this.props.habit);
-    //const name = this.props.habit.name;
-    //const count = this.props.habit.count;
     const { name, count } = this.props.habit;
     return (
     <li className="habit">  
@@ -33,7 +26,7 @@ class Habit extends Component {
       <button className="habit-button habit-decrease" onClick={this.handleDecrement}>
         <i className="fas fa-minus-square"></i>
       </button>
-      <button className="habit-button habit-delete">
+      <button className="habit-button habit-delete" onClick={this.handleDelete}>
         <i className="fas fa-trash"></i>
       </button>
     </li>
