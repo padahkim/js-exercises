e763fa5fd8c9201fe5025e51f38729710d342c9b
padahkim <padahkim@gmail.com>
2021-12-02 16:30:08 +0900

    finish habit tracker1

--- habit-tracker/src/components/habits.jsx
+++ habit-tracker/src/components/habits.jsx
@@ -1,52 +1,39 @@
 import React, { Component } from 'react';
 import Habit from './habit'
+import HabitAddForm from './habitAddForm';
 
 class Habits extends Component {
-  handleIncrement = (element) => {
-    const habits = [...this.state.habits];
-    const index = habits.indexOf(element);
-    habits[index].count++;
-    this.setState({ habits });
+  handleIncrement = (habit) => {
+    this.props.onIncrement(habit);
   };
 
   handleDecrement = (habit) => {
-    const habits = [...this.state.habits];
-    const index = habits.indexOf(habit);
-    const count = habits[index].count;
-    //habits[index].count > 0 ? habits[index].count -1 : 0;
-    //habits[index].count = habits[index].count > 0 ? habits[index].count - 1 : 0;
-    habits[index].count = count > 0 ? count - 1 : 0;
-    this.setState({ habits });
+    this.props.onDecrement(habit);
   };
 
   handleDelete = (habit) => {
-    const habits = this.state.habits.filter(item=>item.id !== habit.id)
-    /*  let habits = [...this.state.habits];
-    const index = habits.indexOf(habit,1);
-    habits = habits.splice(index,2)  */
-    this.setState( { habits } );
+    this.props.onDelete(habit);
+  };
+  handleAdd = (name) => {
+    this.props.onAdd(name);
   };
 
-  state = {
-    habits:[
-      { id:1, name: 'Reading', count: 0 },
-      { id:2, name: 'Running', count: 0 },
-      { id:3, name: 'Coding', count: 0 },
-    ],
-  };
   render() {
-    return (
+    return (<>
+    <HabitAddForm onAdd={this.handleAdd}/>
     <ul>
-      {this.state.habits.map(habit => (//habits []의 각 엘리먼트를 <Habit/>으로 변환 한 배열을 리턴
+      {this.props.habits.map(habit => (//habits []의 각 엘리먼트를 <Habit/>으로 변환 한 배열을 리턴
         <Habit 
-          key={habit.id} 
-          habit={habit} 
+          key = {habit.id}
+          habit = {habit}
           onIncrement={this.handleIncrement} 
           onDecrement={this.handleDecrement} 
           onDelete={this.handleDelete}
           />
         ))}
     </ul>
+    <button className="habits-reset" onClick={this.props.onReset}>Reset All</button>
+    </>
     );
   }
 }
---
+++ habit-tracker/src/components/navbar.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class Navbar extends Component {
+  render() {
+    //const count = {props}
+
+
+    return(
+      <nav className="navbar">
+        <i className="navbar-logo fas fa-leaf"></i>
+        <span>Habit Tracker</span>
+        <span className="navbar-count">{this.props.totalCount}</span>
+      </nav>
+    )
+  }
+}
+
+
+export default Navbar;
