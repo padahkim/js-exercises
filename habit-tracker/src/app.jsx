13c66c1121c3a71dd5eb28d54ca77c08fed3ec91
padahkim <padahkim@gmail.com>
2021-12-05 21:38:35 +0900

    lifting state up to app & add navbar

--- habit-tracker/src/app.jsx
+++ habit-tracker/src/app.jsx
@@ -1,9 +1,57 @@
-import React from 'react';
+import React , { Component } from 'react';
 import './app.css';
 import Habits from'./components/habits'
+import Navbar from './components/navbar';
 
-function App() {
-  return <Habits />;
+class App extends Component {
+  state = {
+    habits: [
+      {id:1, title:"Reading", count: 0 },
+      {id:2, title:"Working", count: 0 },
+      {id:3, title:"Running", count: 0 }, 
+    ],
+  };
+
+  handleIncrement = (habit) => {
+    const habits = [...this.state.habits];
+    const index = habits.indexOf(habit);
+    habits[index].count++;
+    this.setState({habits});//{habits:habits}한거임
+  };
+
+  handleDecrement = (habit) => {
+    const habits = [...this.state.habits];
+    const index = habits.indexOf(habit);
+    habits[index].count = habits[index].count >= 1 ? habits[index].count - 1 : 0;
+    /* let count = habits[index].count;
+    count = count >= 1 ? count - 1 : 0;  이건 안됨,,,*/ 
+    this.setState({habits})
+  };
+
+  handleDelete = (habit) => {
+  /*  const habits = this.state.habits.filter((item)=>item === habit);
+    this.setState({habits}) */
+    const habits = [...this.state.habits];
+    const index = habits.indexOf(habit);
+    const newHabits = habits.filter((habit)=>habit.id!==habits[index].id)  
+    this.setState({habits:newHabits})
+  };
+
+  render() {
+    return (
+      <>
+      <Navbar
+        totalCount={this.state.habits.filter((habit)=>habit.count > 0).length}
+      />
+      <Habits 
+      habits={this.state.habits}
+      onIncrement={this.handleIncrement}
+      onDecrement={this.handleDecrement}
+      onDelete={this.handleDelete}
+      />
+      </>
+      )
+  }
 }
 
 export default App;
--- habit-tracker/src/components/habits.jsx
+++ habit-tracker/src/components/habits.jsx
@@ -3,50 +3,18 @@ import Habit from './habit'
 
 class Habits extends Component {
 
-  state = {
-    habits: [
-      {id:1, title:"Reading", count: 0 },
-      {id:2, title:"Working", count: 0 },
-      {id:3, title:"Running", count: 0 }, 
-    ],
-  };
-
-  handleIncrement = (habit) => {
-    const habits = [...this.state.habits];
-    const index = habits.indexOf(habit);
-    habits[index].count++;
-    this.setState({habits});//{habits:habits}한거임
-  };
-
-  handleDecrement = (habit) => {
-    const habits = [...this.state.habits];
-    const index = habits.indexOf(habit);
-    habits[index].count = habits[index].count >= 1 ? habits[index].count - 1 : 0;
-    /* let count = habits[index].count;
-    count = count >= 1 ? count - 1 : 0;  이건 안됨,,,*/ 
-    this.setState({habits})
-  };
-
-  handleDelete = (habit) => {
-  /*  const habits = this.state.habits.filter((item)=>item === habit);
-    this.setState({habits}) */
-    const habits = [...this.state.habits];
-    const index = habits.indexOf(habit);
-    const newHabits = habits.filter((habit)=>habit.id!==habits[index].id)  
-    this.setState({habits:newHabits})
-  };
 
   render() {
     return (
       <ul>
         {
-          this.state.habits.map((habit)=>(
+          this.props.habits.map((habit)=>(
             <Habit 
             key={habit.id} 
             habit={habit}
-            onIncrement={this.handleIncrement}
-            onDecrement={this.handleDecrement}
-            onDelete={this.handleDelete}
+            onIncrement={this.props.onIncrement}
+            onDecrement={this.props.onDecrement}
+            onDelete={this.props.onDelete}
             />))
         }
       </ul>
