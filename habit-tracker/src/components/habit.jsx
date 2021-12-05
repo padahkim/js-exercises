2ac3ed6673fb5d64e404c87912aec0df76cf1926
padahkim <padahkim@gmail.com>
2021-12-05 14:26:39 +0900

    implement all buttons function

--- habit-tracker/src/components/habit.jsx
+++ habit-tracker/src/components/habit.jsx
@@ -1,30 +1,26 @@
 import React, { Component } from 'react'
 
 class Habit extends Component {
-
-  state = {
-    count: 0,
-  }
-
+  
   handleIncrement = () => {
-    this.setState({ count:this.state.count+1})
-  }
+    this.props.onIncrement(this.props.habit);
+  };
 
   handleDecrement = () => {
-    this.setState({ count:this.state.count > 0 ? this.state.count-1 : 0})
-  }
+    this.props.onDecrement(this.props.habit);
+  };
 
   handleDelete = () => {
-  }
+    this.props.onDelete(this.props.habit);
+  };
 
   render() {
-    console.log(this.props.habit)
     const {title, count} = this.props.habit;
     return (
     <>
     <li className="habit">
       <span className="habit-name">{title}</span>
-      <span className="habit-count">{this.state.count}</span>
+      <span className="habit-count">{count}</span>
       <button className="habit-button habit-increase" onClick={this.handleIncrement}>
         <i className="fas fa-plus-square"></i>
       </button>
@@ -44,3 +40,4 @@ export default Habit;
 //{this.props.name}나의 뇌절코딩1
 //{this.state.count}발전된 뇌절- 버튼누르면 화면상에 값이 변함/count값도 잘변함
 //{this.props.count}버튼누르면 화면상에 값은 안변함 but props내의 count값은 변함
+//위에것들은 원래 안되는 것들이었슴.onClick={this.props.onIncrement(this.props.habit)}이건 왜 안될까...
